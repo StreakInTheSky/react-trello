7183d2592183fb2f2fabee087988d35d8d698814
StreakInTheSky <streakinthesky@gmail.com>
2017-04-14 19:49:21 -0700

    create components

---
+++ js/components/board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './list';
+
+export default function Board() {
+  const lists = [];
+  for (let i=0; i<3; i++) {
+    lists.push(<List />);
+  }
+  return (
+    <div>{lists}</div>
+  );
+}
